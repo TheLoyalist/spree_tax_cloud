83ef2e30462ea6f072955c308070435a8b098f84
Mariusz Roliński <mariusz@mondoreale.pl>
2019-02-25 17:47:40 +0100

    If it throws it’s not gonna return

--- app/models/spree/calculator/tax_cloud_calculator.rb
+++ app/models/spree/calculator/tax_cloud_calculator.rb
@@ -15,27 +15,26 @@ module Spree
     def compute_shipment_or_line_item(item)
       if rate.included_in_price
         raise 'TaxCloud cannot calculate inclusive sales taxes.'
-      else
-        round_to_two_places(tax_for_item(item))
-        # TODO take discounted_amount into account. This is a problem because TaxCloud API does not take discounts nor does it return percentage rates.
       end
-    end
 
+      round_to_two_places(tax_for_item(item))
+      # TODO take discounted_amount into account. This is a problem because TaxCloud API does not take discounts nor does it return percentage rates.
+    end
     alias_method :compute_shipment, :compute_shipment_or_line_item
     alias_method :compute_line_item, :compute_shipment_or_line_item
 
     def compute_shipping_rate(shipping_rate)
       if rate.included_in_price
         raise 'TaxCloud cannot calculate inclusive sales taxes.'
-      else
-        # Sales tax will be applied to the Shipment itself, rather than to the Shipping Rates.
-        # Note that this method is called from ShippingRate.display_price, so if we returned
-        # the shipping sales tax here, it would display as part of the display_price of the 
-        # ShippingRate, which is not consistent with how US sales tax typically works -- i.e.,
-        # it is an additional amount applied to a sale at the end, rather than being part of
-        # the displayed cost of a good or service.
-        return 0
       end
+
+      # Sales tax will be applied to the Shipment itself, rather than to the Shipping Rates.
+      # Note that this method is called from ShippingRate.display_price, so if we returned
+      # the shipping sales tax here, it would display as part of the display_price of the 
+      # ShippingRate, which is not consistent with how US sales tax typically works -- i.e.,
+      # it is an additional amount applied to a sale at the end, rather than being part of
+      # the displayed cost of a good or service.
+      0
     end
 
     private
@@ -43,8 +42,11 @@ module Spree
     def tax_for_item(item)
       order = item.order
       item_address = order.ship_address || order.bill_address
-      # Only calculate tax when we have an address and it's in our jurisdiction
-      return 0 unless item_address.present? && calculable.zone.include?(item_address)
+
+      unless item_address.present? && calculable.zone.include?(item_address)
+        # Only calculate tax when we have an address and it's in our jurisdiction
+        return 0
+      end
 
       # Cache will expire if the order, any of its line items, or any of its shipments change.
       # When the cache expires, we will need to make another API call to TaxCloud.
