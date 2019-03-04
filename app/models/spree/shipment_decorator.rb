e2a5032e48cf285f6351f7d2759468ac38099b5b
Mariusz Roliński <mariusz@mondoreale.pl>
2019-03-04 14:22:36 +0100

    Support multiple origins
    Determines multiple TaxCloud transactions,
    unfortunatelly.

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,5 +1,27 @@
 Spree::Shipment.class_eval do
   def tax_cloud_cache_key
     "#{self.cache_key}--from:#{self.stock_location.cache_key}--to:#{self.order.shipping_address.cache_key}"
-  end  
+  end
+
+  def tax_cloud_items
+    line_items.map do |line_item|
+      Spree::TaxCloud::Item.new(
+        line_item.id,
+        line_item.class.name,
+        stock_location_id,
+        line_item.price,
+        line_item.product&.tax_cloud_tic.presence || Spree::Config.taxcloud_default_product_tic,
+        inventory_units_for_item(line_item).count,
+      )
+    end + [
+      Spree::TaxCloud::Item.new(
+        number,
+        self.class.name,
+        stock_location_id,
+        cost,
+        Spree::Config.taxcloud_shipping_tic,
+        1,
+      )
+    ]
+  end
 end
--- app/models/spree/tax_cloud.rb
+++ app/models/spree/tax_cloud.rb
@@ -1,31 +1,13 @@
 module Spree
   class TaxCloud
+    Item = Struct.new(:id, :type, :stock_location_id, :price, :tic, :quantity)
+
     def self.reload_config
       ::TaxCloud.configure do |config|
         config.api_login_id = Spree::Config.taxcloud_api_login_id
         config.api_key = Spree::Config.taxcloud_api_key
         config.usps_username = Spree::Config.taxcloud_usps_user_id
       end
-    end
-
-    def self.transaction_from_order(order)
-      stock_location = order.shipments.first.try(:stock_location) || Spree::StockLocation.active.where("city IS NOT NULL and state_id IS NOT NULL").first
-      raise Spree.t(:ensure_one_valid_stock_location) unless stock_location
-
-      transaction = ::TaxCloud::Transaction.new(
-        customer_id: order.user_id || order.email,
-        order_id: order.number,
-        cart_id: order.number,
-        origin: address_from_spree_address(stock_location),
-        destination: address_from_spree_address(order.ship_address || order.bill_address)
-      )
-
-      index = -1 # array is zero-indexed
-      # Prepare line_items for lookup
-      order.line_items.each { |line_item| transaction.cart_items << cart_item_from_item(line_item, index += 1) }
-      # Prepare shipments for lookup
-      order.shipments.each { |shipment| transaction.cart_items << cart_item_from_item(shipment, index += 1) }
-      transaction
     end
 
     # Note that this method can take either a Spree::StockLocation (which has address
@@ -35,31 +17,70 @@ module Spree
         address1: address.address1,
         address2: address.address2,
         city:     address.city,
-        state:    address.try(:state).try(:abbr),
+        state:    address&.state&.abbr,
         zip5:     address.zipcode.try(:[], 0...5)
       )
     end
 
-    def self.cart_item_from_item(item, index)
-      case item
-      when Spree::LineItem
-        ::TaxCloud::CartItem.new(
-          index:    index,
-          item_id:  item.try(:variant).try(:sku).present? ? item.try(:variant).try(:sku) : "LineItem #{item.id}",
-          tic:      (item.product.tax_cloud_tic || Spree::Config.taxcloud_default_product_tic),
-          price:    item.price,
-          quantity: item.quantity
-        )
-      when Spree::Shipment
-        ::TaxCloud::CartItem.new(
-          index:    index,
-          item_id:  "Shipment #{item.number}",
-          tic:      Spree::Config.taxcloud_shipping_tic,
-          price:    item.cost,
-          quantity: 1
-        )
-      else
-        raise Spree.t(:cart_item_cannot_be_made)
+    def self.each_transaction(order)
+      Enumerator.new do |y|
+        item_groups = order.tax_cloud_combined_items.group_by(&:stock_location_id).values
+
+        transactions = item_groups.each do |items|
+          stock_location = Spree::StockLocation.find(items.first.stock_location_id)
+
+          transaction = ::TaxCloud::Transaction.new(
+            customer_id: order.user_id || order.email,
+            order_id: order.number,
+            cart_id: "#{order.number}/#{stock_location.id}",
+            origin: address_from_spree_address(stock_location),
+            destination: address_from_spree_address(order.ship_address || order.bill_address)
+          )
+
+          transaction.cart_items = items.map.with_index do |item, index|
+            ::TaxCloud::CartItem.new({
+              index: index,
+              item_id: item.id,
+              tic: item.tic,
+              price: item.price,
+              quantity: item.quantity,
+            })
+          end
+
+          y << [transaction, items]
+        end
+      end
+    end
+
+    def self.lookup(item)
+      # Cache will expire if the order, any of its line items, or any of its shipments change.
+      # When the cache expires, we will need to make another API call to TaxCloud.
+      Rails.cache.fetch(['TaxCloudRatesForItem', item.tax_cloud_cache_key], time_to_idle: 5.minutes) do
+        # In the case of a cache miss, we recompute the amounts for _all_ the LineItems and Shipments for this Order.
+
+        tax_amounts = each_transaction(item.order).reduce({
+          'Spree::LineItem' => {},
+          'Spree::Shipment' => {},
+        }) do |memo, (transaction, items)|
+          cart_items = transaction.lookup.cart_items
+
+          items.each_with_index do |item, index|
+            memo[item.type][item.id] ||= 0
+            memo[item.type][item.id] += cart_items[index].tax_amount
+          end
+
+          memo
+        end
+
+        tax_amounts['Spree::LineItem'].each do |id, tax_amount|
+          Rails.cache.write(['TaxCloudRatesForItem', Spree::LineItem.find(id).tax_cloud_cache_key], tax_amount, time_to_idle: 5.minutes)
+        end
+
+        tax_amounts['Spree::Shipment'].each do |number, tax_amount|
+          Rails.cache.write(['TaxCloudRatesForItem', Spree::Shipment.find_by!(number: number).tax_cloud_cache_key], tax_amount, time_to_idle: 5.minutes)
+        end
+
+        Rails.cache.read(['TaxCloudRatesForItem', item.tax_cloud_cache_key])
       end
     end
   end
