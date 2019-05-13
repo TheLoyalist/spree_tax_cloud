b85fb5ca897eee14ab09a99414f14c4de35836c8
Mariusz Roliński <mariusz@mondoreale.pl>
2019-05-13 23:03:45 +0200

    We’re gonna deal with capturing in the host app; skipping it here

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,15 +1,4 @@
 Spree::Order.class_eval do
-  self.state_machine.after_transition to: :complete, do: :capture_tax_cloud
-
-  def capture_tax_cloud
-    return unless is_taxed_using_tax_cloud?
-    response =  Spree::TaxCloud.transaction_from_order(self).authorized_with_capture
-    if response != "OK"
-      Rails.logger.error "ERROR: TaxCloud returned an order capture response of #{response}."
-    end
-    log_tax_cloud(response)
-  end
-
   # TaxRate.match is used here to check if the order is taxable by Tax Cloud.
   # It's not possible check against the order's tax adjustments because
   # an adjustment is not created for 0% rates. However, US orders must be
